b4bd3dd003d422a158d4010aa6b0d5abd746dee9
邹永赫 <1259085392@qq.com>
2026-07-22 03:01:17 +0900

    build: verify Android bootstrap adapter
    Align Android compilation with API 36 and JVM 17.

--- android/termux-android/build.gradle.kts
+++ android/termux-android/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.library")
     kotlin("android")
@@ -5,10 +7,21 @@ plugins {
 
 android {
     namespace = "com.termux.rust"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
